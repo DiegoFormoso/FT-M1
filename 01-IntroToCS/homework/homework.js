6e4825aa2bc7e708ee937a91602a48a373939b6a
DiegoFormoso <dformoso@yahoo.com>
2022-08-02 20:54:18 -0300

    intro

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,13 +1,22 @@
 'use strict'
 
 function BinarioADecimal(num) {
-  // tu codigo aca
-
+  var numbin = num.toString().split("").reverse();
+  var result = 0;
+  for (var i=0; i<numbin.length; i++) {
+      result = result + numbin[i] * Math.pow(2, i);
+  }
+  return result;
 }
 
 function DecimalABinario(num) {
-  // tu codigo aca
-
+  var numero = num;
+  var binario = (num % 2).toString();
+  while (numero > 1) {
+    numero = parseInt(numero / 2);
+    binario = (numero % 2).toString() + binario;
+  }
+  return binario;
 }
 
 
